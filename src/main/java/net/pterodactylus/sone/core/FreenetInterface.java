ffd5ab370b2f665efd3c9cf129e59aef30b9bace
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2010-10-12 14:10:01 +0200

    Add freenet interface stub.

---
+++ src/main/java/net/pterodactylus/sone/core/FreenetInterface.java
@@ -0,0 +1,43 @@
+/*
+ * FreenetSone - FreenetInterface.java - Copyright © 2010 David Roden
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.pterodactylus.sone.core;
+
+import net.pterodactylus.util.service.AbstractService;
+import freenet.node.Node;
+
+/**
+ * Contains all necessary functionality for interacting with the Freenet node.
+ *
+ * @author <a href="mailto:bombe@pterodactylus.net">David ‘Bombe’ Roden</a>
+ */
+public class FreenetInterface extends AbstractService {
+
+	/** The node to interact with. */
+	private final Node node;
+
+	/**
+	 * Creates a new Freenet interface.
+	 *
+	 * @param node
+	 *            The node to interact with
+	 */
+	public FreenetInterface(Node node) {
+		this.node = node;
+	}
+
+}
