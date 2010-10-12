24c8c4e46a2badd21147ffec19bc833f1993e3f0
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2010-10-12 06:50:52 +0200

    Add plugin stub.

---
+++ src/main/java/net/pterodactylus/sone/main/SonePlugin.java
@@ -0,0 +1,51 @@
+/*
+ * FreenetSone - SonePlugin.java - Copyright © 2010 David Roden
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.pterodactylus.sone.main;
+
+import freenet.pluginmanager.FredPlugin;
+import freenet.pluginmanager.PluginRespirator;
+
+/**
+ * This class interfaces with Freenet. It is the class that is loaded by the
+ * node and starts up the whole Sone system.
+ *
+ * @author <a href="mailto:bombe@pterodactylus.net">David ‘Bombe’ Roden</a>
+ */
+public class SonePlugin implements FredPlugin {
+
+	//
+	// FREDPLUGIN METHODS
+	//
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void runPlugin(PluginRespirator pluginRespirator) {
+		/* TODO */
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void terminate() {
+		/* TODO */
+	}
+
+}
