d4c0238c0e479a3d08390de0e71c1d29beda00e7
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2010-10-13 06:30:44 +0200

    Add login page.

---
+++ src/main/java/net/pterodactylus/sone/web/LoginPage.java
@@ -0,0 +1,41 @@
+/*
+ * FreenetSone - LoginPage.java - Copyright © 2010 David Roden
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.pterodactylus.sone.web;
+
+import net.pterodactylus.util.template.Template;
+
+/**
+ * The login page manages logging the user in.
+ *
+ * @author <a href="mailto:bombe@pterodactylus.net">David ‘Bombe’ Roden</a>
+ */
+public class LoginPage extends SoneTemplatePage {
+
+	/**
+	 * Creates a new login page.
+	 *
+	 * @param template
+	 *            The template to render
+	 * @param webInterface
+	 *            The Sone web interface
+	 */
+	public LoginPage(Template template, WebInterface webInterface) {
+		super("login.html", template, "Page.Login.Title", webInterface);
+	}
+
+}
