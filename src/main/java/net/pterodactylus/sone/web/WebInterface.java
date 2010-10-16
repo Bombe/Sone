fd30c0adf935d90f496671d9122f5ab0b9043670
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2010-10-16 02:57:14 +0200

    Add “view post” page.

--- src/main/java/net/pterodactylus/sone/web/WebInterface.java
+++ src/main/java/net/pterodactylus/sone/web/WebInterface.java
@@ -172,6 +172,9 @@ public class WebInterface extends AbstractService {
 		Template viewSoneTemplate = templateFactory.createTemplate(createReader("/templates/viewSone.html"));
 		viewSoneTemplate.set("formPassword", formPassword);
 
+		Template viewPostTemplate = templateFactory.createTemplate(createReader("/templates/viewPost.html"));
+		viewPostTemplate.set("formPassword", formPassword);
+
 		Template followSoneTemplate = templateFactory.createTemplate(createReader("/templates/followSone.html"));
 		followSoneTemplate.set("formPassword", formPassword);
 
@@ -187,6 +190,7 @@ public class WebInterface extends AbstractService {
 		pageToadlets.add(pageToadletFactory.createPageToadlet(new CreatePostPage(createPostTemplate, this)));
 		pageToadlets.add(pageToadletFactory.createPageToadlet(new CreateReplyPage(createReplyTemplate, this)));
 		pageToadlets.add(pageToadletFactory.createPageToadlet(new ViewSonePage(viewSoneTemplate, this)));
+		pageToadlets.add(pageToadletFactory.createPageToadlet(new ViewPostPage(viewPostTemplate, this)));
 		pageToadlets.add(pageToadletFactory.createPageToadlet(new FollowSonePage(followSoneTemplate, this)));
 		pageToadlets.add(pageToadletFactory.createPageToadlet(new DeleteSonePage(deleteSoneTemplate, this), "DeleteSone"));
 		pageToadlets.add(pageToadletFactory.createPageToadlet(new LoginPage(loginTemplate, this), "Login"));
