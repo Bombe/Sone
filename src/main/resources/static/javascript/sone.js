0cee1b73918990cc5823e1e938605e3a6c644837
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2010-10-19 21:01:18 +0200

    Redesign and consolidate.

--- src/main/resources/static/javascript/sone.js
+++ src/main/resources/static/javascript/sone.js
@@ -16,3 +16,32 @@ function registerInputTextareaSwap(inputSelector, defaultText) {
 		}
 	});
 }
+
+/* hide all the “create reply” forms until a link is clicked. */
+function addCommentLinks() {
+	$("#sone .post").each(function() {
+		postId = $(this).attr("id");
+		commentElement = (function(postId) {
+			var commentElement = $("<div>Comment</div>").addClass("show-reply-form").click(function() {
+				replyElement = $("#sone .post#" + postId + " .create-reply");
+				replyElement.removeClass("hidden");
+				replyElement.removeClass("light");
+				(function(replyElement) {
+					replyElement.find("input.reply-input").blur(function() {
+						if ($(this).hasClass("default")) {
+							replyElement.addClass("light");
+						}
+					}).focus(function() {
+						replyElement.removeClass("light");
+					});
+				})(replyElement);
+				replyElement.find("input.reply-input").focus();
+			});
+			return commentElement;
+		})(postId);
+		$(this).find(".create-reply").addClass("hidden");
+		$(this).find(".status-line .time").each(function() {
+			$(this).after(commentElement.clone(true));
+		});
+	});
+}
