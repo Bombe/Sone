067571ca9d47d10dd401312f6bfe367908866f28
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2010-10-21 14:36:56 +0200

    Move most of the JavaScript to sone.js.

--- src/main/resources/static/javascript/sone.js
+++ src/main/resources/static/javascript/sone.js
@@ -56,3 +56,68 @@ function addCommentLinks() {
 		});
 	});
 }
+
+/**
+ * Fires off an AJAX request to retrieve the current status of a Sone.
+ *
+ * @param soneId
+ *            The ID of the Sone
+ */
+function getSoneStatus(soneId) {
+	$.getJSON("ajax/getSoneStatus.ajax", {"sone": soneId}, function(data, textStatus) {
+		updateSoneStatus(soneId, data.status, data.modified, data.lastUpdated);
+		/* seconds! */
+		updateInterval = 60;
+		if (data.age < 600) {
+			updateInterval = 5;
+		} else if (data.age < 86400) {
+			updateInterval = 30;
+		}
+		setTimeout(function() {
+			getSoneStatus(soneId);
+		}, updateInterval * 1000);
+	});
+}
+
+/**
+ * Updates the status of the given Sone.
+ *
+ * @param soneId
+ *            The ID of the Sone to update
+ * @param status
+ *            The status of the Sone (“idle”, “unknown”, “inserting”,
+ *            “downloading”)
+ * @param modified
+ *            Whether the Sone is modified
+ * @param lastUpdated
+ *            The date and time of the last update (formatted for display)
+ */
+function updateSoneStatus(soneId, status, modified, lastUpdated) {
+	$("#sone .sone#" + soneId).
+		toggleClass("unknown", status == "unknown").
+		toggleClass("idle", status == "idle").
+		toggleClass("inserting", status == "inserting").
+		toggleClass("downloading", status == "downloading").
+		toggleClass("modified", modified);
+	$("#sone .sone#" + soneId + " .last-update span.time").text(lastUpdated);
+}
+
+var watchedSones = {};
+
+/**
+ * Watches this Sone for updates to its status.
+ *
+ * @param soneId
+ *            The ID of the Sone to watch
+ */
+function watchSone(soneId) {
+	if (watchedSones[soneId]) {
+		return;
+	}
+	watchedSones[soneId] = true;
+	(function(soneId) {
+		setTimeout(function() {
+			getSoneStatus(soneId);
+		}, 5000);
+	})(soneId);
+}
