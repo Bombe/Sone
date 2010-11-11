cb54e80848d2511f8090b584eb56a8e80df589c5
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2010-11-11 22:11:24 +0100

    Set version to 0.2.1.

--- src/main/java/net/pterodactylus/sone/main/SonePlugin.java
+++ src/main/java/net/pterodactylus/sone/main/SonePlugin.java
@@ -81,7 +81,7 @@ public class SonePlugin implements FredPlugin, FredPluginL10n, FredPluginBaseL10
 	}
 
 	/** The version. */
-	public static final Version VERSION = new Version(0, 2);
+	public static final Version VERSION = new Version(0, 2, 1);
 
 	/** The logger. */
 	private static final Logger logger = Logging.getLogger(SonePlugin.class);
