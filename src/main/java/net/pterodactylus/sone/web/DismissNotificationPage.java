75771a85377be48674caeedca8253c9c3dbdf818
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2010-11-14 15:38:48 +0100

    Add page that dismisses a notification.

---
+++ src/main/java/net/pterodactylus/sone/web/DismissNotificationPage.java
@@ -0,0 +1,61 @@
+/*
+ * Sone - DismissNotificationPage.java - Copyright © 2010 David Roden
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.pterodactylus.sone.web;
+
+import net.pterodactylus.util.notify.Notification;
+import net.pterodactylus.util.template.Template;
+
+/**
+ * Page that lets the user dismiss a notification.
+ *
+ * @author <a href="mailto:bombe@pterodactylus.net">David ‘Bombe’ Roden</a>
+ */
+public class DismissNotificationPage extends SoneTemplatePage {
+
+	/**
+	 * Creates a new “dismiss notifcation” page.
+	 *
+	 * @param template
+	 *            The template to render
+	 * @param webInterface
+	 *            The Sone web interface
+	 */
+	public DismissNotificationPage(Template template, WebInterface webInterface) {
+		super("dismissNotification.html", template, "Page.DismissNotification.Title", webInterface);
+	}
+
+	//
+	// TEMPLATEPAGE METHODS
+	//
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void processTemplate(Request request, Template template) throws RedirectException {
+		super.processTemplate(request, template);
+		String notificationId = request.getHttpRequest().getPartAsStringFailsafe("notification", 36);
+		Notification notification = webInterface.getCore().getNotifications().getNotification(notificationId);
+		if ((notification != null) && notification.isDismissable()) {
+			webInterface.getCore().getNotifications().removeNotification(notification);
+		}
+		String returnPage = request.getHttpRequest().getPartAsStringFailsafe("returnPage", 64);
+		throw new RedirectException(returnPage);
+	}
+
+}
