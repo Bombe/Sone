728eb9dfab6b9284dbb69701fa8ba4a9b072e7ca
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2010-11-19 21:53:49 +0100

    Don’t always create a Sone for a given ID.

--- src/main/java/net/pterodactylus/sone/web/ViewSonePage.java
+++ src/main/java/net/pterodactylus/sone/web/ViewSonePage.java
@@ -50,7 +50,7 @@ public class ViewSonePage extends SoneTemplatePage {
 	protected void processTemplate(Request request, Template template) throws RedirectException {
 		super.processTemplate(request, template);
 		String soneId = request.getHttpRequest().getParam("sone");
-		Sone sone = webInterface.getCore().getSone(soneId);
+		Sone sone = webInterface.getCore().getSone(soneId, false);
 		template.set("sone", sone);
 	}
 
