43cfb3293651b162c4b2d7aac4c5ecaf4efc3fc2
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2011-01-01 14:31:53 +0100

    Add Fingerprintable interface.

--- src/main/java/net/pterodactylus/sone/data/Sone.java
+++ src/main/java/net/pterodactylus/sone/data/Sone.java
@@ -40,7 +40,7 @@ import freenet.keys.FreenetURI;
  *
  * @author <a href="mailto:bombe@pterodactylus.net">David ‘Bombe’ Roden</a>
  */
-public class Sone {
+public class Sone implements Fingerprintable {
 
 	/** comparator that sorts Sones by their nice name. */
 	public static final Comparator<Sone> NICE_NAME_COMPARATOR = new Comparator<Sone>() {
@@ -580,14 +580,14 @@ public class Sone {
 		return this;
 	}
 
+	//
+	// FINGERPRINTABLE METHODS
+	//
+
 	/**
-	 * Returns a fingerprint of this Sone. The fingerprint only depends on data
-	 * that is actually stored when a Sone is inserted. The fingerprint can be
-	 * used to detect changes in Sone data and can also be used to detect if
-	 * previous changes are reverted.
-	 *
-	 * @return The fingerprint of this Sone
+	 * {@inheritDoc}
 	 */
+	@Override
 	public synchronized String getFingerprint() {
 		StringBuilder fingerprint = new StringBuilder();
 		fingerprint.append("Profile(");
