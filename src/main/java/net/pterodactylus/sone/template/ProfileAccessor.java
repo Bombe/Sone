2cee36b47bf421ab43be6ca5a6be7fdb67441418
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2011-01-10 12:10:39 +0100

    Add accessor for profile fields.

---
+++ src/main/java/net/pterodactylus/sone/template/ProfileAccessor.java
@@ -0,0 +1,51 @@
+/*
+ * Sone - ProfileAccessor.java - Copyright © 2011 David Roden
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.pterodactylus.sone.template;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import net.pterodactylus.sone.data.Profile;
+import net.pterodactylus.util.template.Accessor;
+import net.pterodactylus.util.template.DataProvider;
+import net.pterodactylus.util.template.ReflectionAccessor;
+
+/**
+ * {@link Accessor} for {@link Profile}s, especially for the profile fields.
+ *
+ * @author <a href="mailto:bombe@pterodactylus.net">David ‘Bombe’ Roden</a>
+ */
+public class ProfileAccessor extends ReflectionAccessor {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Object get(DataProvider dataProvider, Object object, String member) {
+		Profile profile = (Profile) object;
+		if ("fields".equals(member)) {
+			Map<String, String> fields = new LinkedHashMap<String, String>();
+			for (String field : profile.getFields()) {
+				fields.put(field, profile.getField(field));
+			}
+			return fields;
+		}
+		return super.get(dataProvider, object, member);
+	}
+
+}
