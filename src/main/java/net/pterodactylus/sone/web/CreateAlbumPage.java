f56a7d806f0dab6abe6bc0ffba93a5ec19e5abcf
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2011-01-12 20:06:12 +0100

    Add page that lets the user create a new album.

---
+++ src/main/java/net/pterodactylus/sone/web/CreateAlbumPage.java
@@ -0,0 +1,69 @@
+/*
+ * Sone - CreateAlbumPage.java - Copyright © 2011 David Roden
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.pterodactylus.sone.web;
+
+import net.pterodactylus.sone.data.Album;
+import net.pterodactylus.sone.data.Sone;
+import net.pterodactylus.sone.web.page.Page.Request.Method;
+import net.pterodactylus.util.template.DataProvider;
+import net.pterodactylus.util.template.Template;
+
+/**
+ * Page that lets the user create a new album.
+ *
+ * @author <a href="mailto:bombe@pterodactylus.net">David ‘Bombe’ Roden</a>
+ */
+public class CreateAlbumPage extends SoneTemplatePage {
+
+	/**
+	 * Creates a new “create album” page.
+	 *
+	 * @param template
+	 *            The template to render
+	 * @param webInterface
+	 *            The Sone web interface
+	 */
+	public CreateAlbumPage(Template template, WebInterface webInterface) {
+		super("createAlbum.html", template, "Page.CreateAlbum.Title", webInterface, true);
+	}
+
+	//
+	// SONETEMPLATEPAGE METHODS
+	//
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void processTemplate(Request request, DataProvider dataProvider) throws RedirectException {
+		super.processTemplate(request, dataProvider);
+		if (request.getMethod() == Method.POST) {
+			String name = request.getHttpRequest().getPartAsStringFailsafe("name", 64).trim();
+			if (name.length() == 0) {
+				dataProvider.set("nameMissing", true);
+				return;
+			}
+			Sone currentSone = getCurrentSone(request.getToadletContext());
+			String parentId = request.getHttpRequest().getPartAsStringFailsafe("parent", 36);
+			Album parent = webInterface.getCore().getAlbum(parentId, false);
+			Album album = webInterface.getCore().createAlbum(currentSone, parent);
+			throw new RedirectException("imageBrowser.html?album=" + album.getId());
+		}
+	}
+
+}
