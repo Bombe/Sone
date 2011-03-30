109600515d4c561d4219fd5c977b5e205e790519
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2011-03-30 07:29:01 +0200

    Add filter that removes not-downloaded Sones.

--- src/main/java/net/pterodactylus/sone/data/Sone.java
+++ src/main/java/net/pterodactylus/sone/data/Sone.java
@@ -29,6 +29,7 @@ import java.util.logging.Logger;
 
 import net.pterodactylus.sone.freenet.wot.Identity;
 import net.pterodactylus.sone.template.SoneAccessor;
+import net.pterodactylus.util.filter.Filter;
 import net.pterodactylus.util.logging.Logging;
 import freenet.keys.FreenetURI;
 
@@ -54,6 +55,15 @@ public class Sone implements Fingerprintable, Comparable<Sone> {
 			return leftSone.getId().compareToIgnoreCase(rightSone.getId());
 		}
 
+	};
+
+	/** Filter to remove Sones that have not been downloaded. */
+	public static final Filter<Sone> EMPTY_SONE_FILTER = new Filter<Sone>() {
+
+		@Override
+		public boolean filterObject(Sone sone) {
+			return sone.getTime() != 0;
+		}
 	};
 
 	/** The logger. */
