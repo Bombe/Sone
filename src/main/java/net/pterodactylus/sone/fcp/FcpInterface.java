86de0f4478465475dd403ae9daa90aabe0ca3e9c
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2011-04-08 16:20:36 +0200

    Add “GetPosts” FCP command.

--- src/main/java/net/pterodactylus/sone/fcp/FcpInterface.java
+++ src/main/java/net/pterodactylus/sone/fcp/FcpInterface.java
@@ -52,6 +52,7 @@ public class FcpInterface {
 	public FcpInterface(Core core) {
 		commands.put("Version", new VersionCommand());
 		commands.put("GetLocalSones", new GetLocalSonesCommand(core));
+		commands.put("GetPosts", new GetPostsCommand(core));
 		commands.put("GetPostFeed", new GetPostFeedCommand(core));
 	}
 
---
+++ src/main/java/net/pterodactylus/sone/fcp/GetPostsCommand.java
@@ -0,0 +1,63 @@
+/*
+ * Sone - GetPostsCommand.java - Copyright © 2011 David Roden
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.pterodactylus.sone.fcp;
+
+import java.util.Collections;
+import java.util.List;
+
+import net.pterodactylus.sone.core.Core;
+import net.pterodactylus.sone.data.Post;
+import net.pterodactylus.sone.data.Sone;
+import net.pterodactylus.sone.freenet.fcp.FcpException;
+import freenet.support.SimpleFieldSet;
+import freenet.support.api.Bucket;
+
+/**
+ * Implements the “GetPosts” FCP command that returns the list of posts a Sone
+ * made.
+ *
+ * @author <a href="mailto:bombe@pterodactylus.net">David ‘Bombe’ Roden</a>
+ */
+public class GetPostsCommand extends AbstractSoneCommand {
+
+	/**
+	 * Creates a new “GetPosts” FCP command.
+	 *
+	 * @param core
+	 *            The Sone core
+	 */
+	public GetPostsCommand(Core core) {
+		super(core);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Response execute(SimpleFieldSet parameters, Bucket data, AccessType accessType) throws FcpException {
+		Sone sone = getSone(parameters, "Sone");
+		int startPost = getInt(parameters, "StartPost", 0);
+		int maxPosts = getInt(parameters, "MaxPosts", -1);
+		List<Post> posts = sone.getPosts();
+		if (posts.size() < startPost) {
+			return new Response(encodePosts(Collections.<Post> emptyList(), false));
+		}
+		return new Response(encodePosts(sone.getPosts().subList(startPost, (maxPosts == -1) ? posts.size() : Math.min(startPost + maxPosts, posts.size())), true));
+	}
+
+}
