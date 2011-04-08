a50303a787867cdc2449da8846e1e365b771e109
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2011-04-08 15:48:04 +0200

    Add “GetPostFeed” FCP command.

---
+++ src/main/java/net/pterodactylus/sone/fcp/GetPostFeedCommand.java
@@ -0,0 +1,82 @@
+/*
+ * Sone - GetPostFeedCommand.java - Copyright © 2011 David Roden
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.pterodactylus.sone.fcp;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import net.pterodactylus.sone.core.Core;
+import net.pterodactylus.sone.data.Post;
+import net.pterodactylus.sone.data.Sone;
+import net.pterodactylus.sone.freenet.fcp.FcpException;
+import net.pterodactylus.util.filter.Filters;
+import freenet.support.SimpleFieldSet;
+import freenet.support.api.Bucket;
+
+/**
+ * Implementation of an FCP interface for other clients or plugins to
+ * communicate with Sone.
+ *
+ * @author <a href="mailto:bombe@pterodactylus.net">David ‘Bombe’ Roden</a>
+ */
+public class GetPostFeedCommand extends AbstractSoneCommand {
+
+	/**
+	 * Creates a new “GetPostFeed” command.
+	 *
+	 * @param core
+	 *            The core
+	 */
+	public GetPostFeedCommand(Core core) {
+		super(core);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Reply execute(SimpleFieldSet parameters, Bucket data, AccessType accessType) throws FcpException {
+		Sone sone = getSone(parameters, "Sone");
+		int startPost = getInt(parameters, "StartPost", 0);
+		int maxPosts = getInt(parameters, "MaxPosts", -1);
+
+		Set<Post> allPosts = new HashSet<Post>();
+		allPosts.addAll(sone.getPosts());
+		for (String friendSoneId : sone.getFriends()) {
+			if (!getCore().hasSone(friendSoneId)) {
+				continue;
+			}
+			allPosts.addAll(getCore().getSone(friendSoneId).getPosts());
+		}
+		allPosts.addAll(getCore().getDirectedPosts(sone));
+		allPosts = Filters.filteredSet(allPosts, Post.FUTURE_POSTS_FILTER);
+
+		List<Post> sortedPosts = new ArrayList<Post>(allPosts);
+		Collections.sort(sortedPosts, Post.TIME_COMPARATOR);
+
+		if (sortedPosts.size() < startPost) {
+			return new Reply(encodePosts(Collections.<Post> emptyList()));
+		}
+
+		return new Reply(encodePosts(sortedPosts.subList(startPost, (maxPosts == -1) ? sortedPosts.size() : Math.min(startPost + maxPosts, sortedPosts.size()))));
+	}
+
+}
