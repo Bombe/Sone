0b2d63c2848aaa3a59c75bc449bd41dffab39b0c
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2011-05-11 07:13:35 +0200

    Add “GetSone” FCP command.

--- src/main/java/net/pterodactylus/sone/fcp/FcpInterface.java
+++ src/main/java/net/pterodactylus/sone/fcp/FcpInterface.java
@@ -84,6 +84,7 @@ public class FcpInterface {
 		commands.put("Version", new VersionCommand(core));
 		commands.put("GetLocalSones", new GetLocalSonesCommand(core));
 		commands.put("GetSones", new GetSonesCommand(core));
+		commands.put("GetSone", new GetSoneCommand(core));
 		commands.put("GetPost", new GetPostCommand(core));
 		commands.put("GetPosts", new GetPostsCommand(core));
 		commands.put("GetPostFeed", new GetPostFeedCommand(core));
