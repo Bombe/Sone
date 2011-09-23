9972b5cd077528e623ab69d13178c7d38796cd05
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2011-09-24 00:24:34 +0200

    Add methods to reorder albums.

--- src/main/java/net/pterodactylus/sone/data/Album.java
+++ src/main/java/net/pterodactylus/sone/data/Album.java
@@ -152,6 +152,46 @@ public class Album implements Fingerprintable {
 	}
 
 	/**
+	 * Moves the given album up in this album’s albums. If the album is already
+	 * the first album, nothing happens.
+	 *
+	 * @param album
+	 *            The album to move up
+	 * @return The album that the given album swapped the place with, or
+	 *         <code>null</code> if the album did not change its place
+	 */
+	public Album moveAlbumUp(Album album) {
+		Validation.begin().isNotNull("Album", album).check().isEqual("Album Owner", album.sone, sone).isEqual("Album Parent", album.parent, this).check();
+		int oldIndex = albums.indexOf(album);
+		if (oldIndex <= 0) {
+			return null;
+		}
+		albums.remove(oldIndex);
+		albums.add(oldIndex - 1, album);
+		return albums.get(oldIndex);
+	}
+
+	/**
+	 * Moves the given album down in this album’s albums. If the album is
+	 * already the last album, nothing happens.
+	 *
+	 * @param album
+	 *            The album to move down
+	 * @return The album that the given album swapped the place with, or
+	 *         <code>null</code> if the album did not change its place
+	 */
+	public Album moveAlbumDown(Album album) {
+		Validation.begin().isNotNull("Album", album).check().isEqual("Album Owner", album.sone, sone).isEqual("Album Parent", album.parent, this).check();
+		int oldIndex = albums.indexOf(album);
+		if ((oldIndex < 0) || (oldIndex >= (albums.size() - 1))) {
+			return null;
+		}
+		albums.remove(oldIndex);
+		albums.add(oldIndex + 1, album);
+		return albums.get(oldIndex);
+	}
+
+	/**
 	 * Returns the images in this album.
 	 *
 	 * @return The images in this album
