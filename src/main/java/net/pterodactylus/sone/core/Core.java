d8c3ac6bf0ee859cfe5abfe8aaf4bff8a5e91c4e
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2013-01-15 09:58:48 +0100

    Implement post reply provider interface in Core.

--- src/main/java/net/pterodactylus/sone/core/Core.java
+++ src/main/java/net/pterodactylus/sone/core/Core.java
@@ -73,7 +73,7 @@ import freenet.keys.FreenetURI;
  *
  * @author <a href="mailto:bombe@pterodactylus.net">David ‘Bombe’ Roden</a>
  */
-public class Core extends AbstractService implements IdentityListener, UpdateListener, SoneProvider, PostProvider, SoneInsertListener, ImageInsertListener {
+public class Core extends AbstractService implements IdentityListener, UpdateListener, SoneProvider, PostProvider, SoneInsertListener, ImageInsertListener, PostReplyProvider {
 
 	/** The logger. */
 	private static final Logger logger = Logging.getLogger(Core.class);
@@ -608,17 +608,9 @@ public class Core extends AbstractService implements IdentityListener, UpdateLis
 	}
 
 	/**
-	 * Returns the reply with the given ID. If there is no reply with the given
-	 * ID yet, a new one is created, unless {@code create} is false in which
-	 * case {@code null} is returned.
-	 *
-	 * @param replyId
-	 *            The ID of the reply to get
-	 * @param create
-	 *            {@code true} to always return a {@link Reply}, {@code false}
-	 *            to return {@code null} if no reply can be found
-	 * @return The reply, or {@code null} if there is no such reply
+	 * {@inheritDoc}
 	 */
+	@Override
 	public PostReply getPostReply(String replyId, boolean create) {
 		synchronized (replies) {
 			PostReply reply = replies.get(replyId);
