85854c510cd14b1999b0caab8849df28cd2b1355
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2013-02-04 06:36:08 +0100

    Extract abstract post reply builder class.

--- src/main/java/net/pterodactylus/sone/data/impl/AbstractPostReplyBuilder.java
+++ src/main/java/net/pterodactylus/sone/data/impl/AbstractPostReplyBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Sone - PostReplyBuilderImpl.java - Copyright © 2013 David Roden
+ * Sone - AbstractPostReplyBuilder.java - Copyright © 2013 David Roden
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,55 +19,47 @@ package net.pterodactylus.sone.data.impl;
 
 import static com.google.common.base.Preconditions.checkState;
 
-import java.util.UUID;
-
-import net.pterodactylus.sone.data.PostReply;
-import net.pterodactylus.sone.database.PostProvider;
-import net.pterodactylus.sone.database.PostReplyBuilder;
-import net.pterodactylus.sone.database.SoneProvider;
-
 import org.apache.commons.lang.StringUtils;
 
+import net.pterodactylus.sone.database.PostReplyBuilder;
+
 /**
- * {@link PostReplyBuilder} implementation that creates {@link PostReplyImpl}
- * objects.
+ * Abstract {@link PostReplyBuilder} implementation. It stores the state of the
+ * new post and performs validation, implementations only need to implement
+ * {@link #build()}.
  *
  * @author <a href="mailto:bombe@pterodactylus.net">David ‘Bombe’ Roden</a>
  */
-public class PostReplyBuilderImpl extends AbstractPostReplyBuilder {
+public abstract class AbstractPostReplyBuilder extends AbstractReplyBuilder<PostReplyBuilder> implements PostReplyBuilder {
 
-	/** The Sone provider. */
-	private final SoneProvider soneProvider;
-
-	/** The post provider. */
-	private final PostProvider postProvider;
-
-	/**
-	 * Creates a new post reply builder.
-	 *
-	 * @param soneProvider
-	 *            The Sone provider
-	 * @param postProvider
-	 *            The post provider
-	 */
-	public PostReplyBuilderImpl(SoneProvider soneProvider, PostProvider postProvider) {
-		this.soneProvider = soneProvider;
-		this.postProvider = postProvider;
-	}
+	/** The ID of the post the created reply refers to. */
+	protected String postId;
 
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
-	public PostReply build() {
+	public PostReplyBuilder to(String postId) {
+		this.postId = postId;
+		return this;
+	}
+
+	//
+	// PROTECTED METHODS
+	//
+
+	/**
+	 * Validates the state of this post reply builder.
+	 *
+	 * @throws IllegalStateException
+	 *             if the state is not valid for building a new post reply
+	 */
+	protected void validate() throws IllegalStateException {
 		checkState((randomId && (id == null)) || (!randomId && (id != null)), "either random ID nor custom ID must be set");
 		checkState(senderId != null, "sender must not be null");
 		checkState((currentTime && (time == 0)) || (!currentTime && (time >= 0)), "either current time or custom time must be set");
 		checkState(!StringUtils.isBlank(text), "text must not be empty");
 		checkState(postId != null, "post must not be null");
-
-		/* create new post reply. */
-		return new PostReplyImpl(soneProvider, postProvider, randomId ? UUID.randomUUID().toString() : id, senderId, currentTime ? System.currentTimeMillis() : time, text, postId);
 	}
 
 }
