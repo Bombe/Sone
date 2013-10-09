5aa205de9614c32eb24e2fd4c59b93046c87ef08
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2013-10-09 21:46:47 +0200

    Add album builder that can create the “old” album objects.

---
+++ src/main/java/net/pterodactylus/sone/data/impl/AlbumBuilderImpl.java
@@ -0,0 +1,37 @@
+/*
+ * Sone - MemoryAlbumBuilder.java - Copyright © 2013 David Roden
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.pterodactylus.sone.data.impl;
+
+import net.pterodactylus.sone.data.Album;
+import net.pterodactylus.sone.data.AlbumImpl;
+import net.pterodactylus.sone.database.AlbumBuilder;
+
+/**
+ * {@link AlbumBuilder} implementation that creates {@link AlbumImpl} objects.
+ *
+ * @author <a href="mailto:bombe@pterodactylus.net">David ‘Bombe’ Roden</a>
+ */
+public class AlbumBuilderImpl extends AbstractAlbumBuilder {
+
+	@Override
+	public Album build() throws IllegalStateException {
+		validate();
+		return randomId ? new AlbumImpl() : new AlbumImpl(id);
+	}
+
+}
