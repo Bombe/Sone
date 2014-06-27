2d1bc900f1134faefb9701cf91729b9469167d0c
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2014-06-27 21:16:29 +0200

    Copy identity classes from refactoring branch.

--- src/main/java/net/pterodactylus/sone/freenet/wot/DefaultOwnIdentity.java
+++ src/main/java/net/pterodactylus/sone/freenet/wot/DefaultOwnIdentity.java
@@ -45,29 +45,33 @@ public class DefaultOwnIdentity extends DefaultIdentity implements OwnIdentity {
 		this.insertUri = insertUri;
 	}
 
-	/**
-	 * Copy constructor for an own identity.
-	 *
-	 * @param ownIdentity
-	 *            The own identity to copy
-	 */
-	public DefaultOwnIdentity(OwnIdentity ownIdentity) {
-		super(ownIdentity.getId(), ownIdentity.getNickname(), ownIdentity.getRequestUri());
-		this.insertUri = ownIdentity.getInsertUri();
-		setContexts(ownIdentity.getContexts());
-		setProperties(ownIdentity.getProperties());
-	}
-
 	//
 	// ACCESSORS
 	//
 
-	/**
-	 * {@inheritDoc}
-	 */
 	@Override
 	public String getInsertUri() {
 		return insertUri;
+	}
+
+	@Override
+	public OwnIdentity addContext(String context) {
+		return (OwnIdentity) super.addContext(context);
+	}
+
+	@Override
+	public OwnIdentity removeContext(String context) {
+		return (OwnIdentity) super.removeContext(context);
+	}
+
+	@Override
+	public OwnIdentity setProperty(String name, String value) {
+		return (OwnIdentity) super.setProperty(name, value);
+	}
+
+	@Override
+	public OwnIdentity removeProperty(String name) {
+		return (OwnIdentity) super.removeProperty(name);
 	}
 
 	//
--- src/main/java/net/pterodactylus/sone/freenet/wot/Identity.java
+++ src/main/java/net/pterodactylus/sone/freenet/wot/Identity.java
@@ -22,8 +22,6 @@ import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
 
-import javax.annotation.Nullable;
-
 import com.google.common.base.Function;
 
 /**
@@ -35,17 +33,17 @@ import com.google.common.base.Function;
  */
 public interface Identity {
 
-	final Function<Identity, Collection<String>> TO_CONTEXTS = new Function<Identity, Collection<String>>() {
+	public static final Function<Identity, Set<String>> TO_CONTEXTS = new Function<Identity, Set<String>>() {
 		@Override
-		public Collection<String> apply(@Nullable Identity identity) {
-			return (identity == null) ? Collections.<String>emptyList() : identity.getContexts();
+		public Set<String> apply(Identity identity) {
+			return (identity == null) ? Collections.<String>emptySet() : identity.getContexts();
 		}
 	};
 
-	final Function<Identity, Map<String, String>> TO_PROPERTIES = new Function<Identity, Map<String, String>>() {
+	public static final Function<Identity, Map<String, String>> TO_PROPERTIES = new Function<Identity, Map<String, String>>() {
 		@Override
-		public Map<String, String> apply(@Nullable Identity identity) {
-			return (identity == null) ? Collections.<String, String>emptyMap() : identity.getProperties();
+		public Map<String, String> apply(Identity input) {
+			return (input == null) ? Collections.<String, String>emptyMap() : input.getProperties();
 		}
 	};
 
@@ -93,7 +91,7 @@ public interface Identity {
 	 * @param context
 	 *            The context to add
 	 */
-	public void addContext(String context);
+	public Identity addContext(String context);
 
 	/**
 	 * Sets all contexts of this identity.
@@ -109,7 +107,7 @@ public interface Identity {
 	 * @param context
 	 *            The context to remove
 	 */
-	public void removeContext(String context);
+	public Identity removeContext(String context);
 
 	/**
 	 * Returns all properties of this identity.
@@ -135,7 +133,7 @@ public interface Identity {
 	 * @param value
 	 *            The value of the property
 	 */
-	public void setProperty(String name, String value);
+	public Identity setProperty(String name, String value);
 
 	/**
 	 * Sets all properties of this identity.
@@ -151,7 +149,7 @@ public interface Identity {
 	 * @param name
 	 *            The name of the property to remove
 	 */
-	public void removeProperty(String name);
+	public Identity removeProperty(String name);
 
 	/**
 	 * Retrieves the trust that this identity receives from the given own
@@ -174,7 +172,7 @@ public interface Identity {
 	 * @param trust
 	 *            The trust given by the given own identity
 	 */
-	public void setTrust(OwnIdentity ownIdentity, Trust trust);
+	public Identity setTrust(OwnIdentity ownIdentity, Trust trust);
 
 	/**
 	 * Removes trust assignment from the given own identity for this identity.
@@ -183,6 +181,6 @@ public interface Identity {
 	 *            The own identity that removed the trust assignment for this
 	 *            identity
 	 */
-	public void removeTrust(OwnIdentity ownIdentity);
+	public Identity removeTrust(OwnIdentity ownIdentity);
 
 }
