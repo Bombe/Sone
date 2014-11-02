2cacd71d0561c205127d0621f91895a5ca3e0a74
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2014-11-02 10:32:41 +0100

    Add method to call private or otherwise non-accessible getters.

--- src/test/java/net/pterodactylus/sone/TestUtil.java
+++ src/test/java/net/pterodactylus/sone/TestUtil.java
@@ -1,6 +1,8 @@
 package net.pterodactylus.sone;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
 /**
@@ -31,6 +33,20 @@ public class TestUtil {
 			field.setAccessible(true);
 			return (T) field.get(object);
 		} catch (NoSuchFieldException e) {
+			throw new RuntimeException(e);
+		} catch (IllegalAccessException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static <T> T callPrivateMethod(Object object, String methodName) {
+		try {
+			Method method = object.getClass().getDeclaredMethod(methodName, new Class[0]);
+			method.setAccessible(true);
+			return (T) method.invoke(object);
+		} catch (NoSuchMethodException e) {
+			throw new RuntimeException(e);
+		} catch (InvocationTargetException e) {
 			throw new RuntimeException(e);
 		} catch (IllegalAccessException e) {
 			throw new RuntimeException(e);
