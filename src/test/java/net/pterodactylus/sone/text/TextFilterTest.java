98efc01af1fdafeaf17c983d13cca0dedd9d9ba3
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2015-07-10 10:43:57 +0200

    Add test for text filter

---
+++ src/test/java/net/pterodactylus/sone/text/TextFilterTest.java
@@ -0,0 +1,52 @@
+package net.pterodactylus.sone.text;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+/**
+ * JUnit test for {@link TextFilter}.
+ *
+ * @author <a href="mailto:bombe@pterodactylus.net">David ‘Bombe’ Roden</a>
+ */
+public class TextFilterTest {
+
+	@Test
+	public void textFilterCanBeCreated() {
+		new TextFilter();
+	}
+
+	@Test
+	public void textFilterRemovesHttpLinkToSameHost() {
+		String textWithHttpLink = "Some text with an http://foo.bar/link.html in it.";
+		assertThat(TextFilter.filter("foo.bar", textWithHttpLink), is("Some text with an link.html in it."));
+	}
+
+	@Test
+	public void textFilterRemovesHttpsLinkToSameHost() {
+		String textWithHttpLink = "Some text with an https://foo.bar/link.html in it.";
+		assertThat(TextFilter.filter("foo.bar", textWithHttpLink), is("Some text with an link.html in it."));
+	}
+
+	@Test
+	public void textWithoutALinkIsReturnedUnmodified() {
+		String textWithHttpLink = "Some text without a link in it.";
+		assertThat(TextFilter.filter("foo.bar", textWithHttpLink), is("Some text without a link in it."));
+	}
+
+	@Test
+	public void nothingIsRemovedWhenThereIsNoHostHeader() {
+		String textWithHttpLink = "Some text with an https://foo.bar/link.html in it.";
+		assertThat(TextFilter.filter(null, textWithHttpLink), is("Some text with an https://foo.bar/link.html in it."));
+	}
+
+	@Test
+	public void textFilterDoesNotRemoveLinksToDifferentHost() {
+		String textWithHttpLink = "Some text with an https://foo.bar/link.html in it.";
+		assertThat(TextFilter.filter("bar.baz", textWithHttpLink), is("Some text with an https://foo.bar/link.html in it."));
+	}
+
+}
