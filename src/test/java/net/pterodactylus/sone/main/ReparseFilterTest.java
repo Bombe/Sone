89fe86e5fa64ad57b90b9b898e4fc03d29da5a63
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2015-07-18 17:29:17 +0200

    Add reparse filter

---
+++ src/test/java/net/pterodactylus/sone/main/ReparseFilterTest.java
@@ -0,0 +1,29 @@
+package net.pterodactylus.sone.main;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import java.util.Collections;
+
+import net.pterodactylus.util.template.TemplateContext;
+
+import org.junit.Test;
+
+/**
+ * Unit test for {@link ReparseFilter}.
+ *
+ * @author <a href="mailto:bombe@pterodactylus.net">David ‘Bombe’ Roden</a>
+ */
+public class ReparseFilterTest {
+
+	private final ReparseFilter reparseFilter = new ReparseFilter();
+
+	@Test
+	public void filterParsesTemplateCorrectly() {
+		TemplateContext templateContext = new TemplateContext();
+		templateContext.set("bar", "baz");
+		assertThat(reparseFilter.format(templateContext, "foo <% bar>", Collections.<String, Object>emptyMap()),
+				is((Object) "foo baz"));
+	}
+
+}
