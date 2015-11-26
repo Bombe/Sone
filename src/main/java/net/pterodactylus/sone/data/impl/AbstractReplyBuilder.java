c8897b270214c5e3c1e0c5cd4db9ac1821bfbde0
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2015-11-26 21:58:57 +0100

    Change file names in copyright lines

--- src/main/java/net/pterodactylus/sone/data/impl/AbstractReplyBuilder.java
+++ src/main/java/net/pterodactylus/sone/data/impl/AbstractReplyBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Sone - ReplyBuilder.java - Copyright © 2013 David Roden
+ * Sone - AbstractReplyBuilder.java - Copyright © 2013 David Roden
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
