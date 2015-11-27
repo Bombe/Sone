419098bcd6215125408b29e60bd888e60979d37b
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2015-11-27 07:11:56 +0100

    Update years in copyright line

--- src/main/java/net/pterodactylus/sone/core/event/UpdateFoundEvent.java
+++ src/main/java/net/pterodactylus/sone/core/event/UpdateFoundEvent.java
@@ -1,5 +1,5 @@
 /*
- * Sone - UpdateFoundEvent.java - Copyright © 2013 David Roden
+ * Sone - UpdateFoundEvent.java - Copyright © 2013–2015 David Roden
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- src/main/java/net/pterodactylus/sone/database/ImageBuilder.java
+++ src/main/java/net/pterodactylus/sone/database/ImageBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Sone - ImageBuilder.java - Copyright © 2013 David Roden
+ * Sone - ImageBuilder.java - Copyright © 2013–2015 David Roden
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
