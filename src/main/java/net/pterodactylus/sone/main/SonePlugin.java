b4d4c2cda1c87aabfb40c9ae0ee794ebd5a1ab7c
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2016-07-11 19:54:38 +0200

    Change version number to 0.9.5

--- src/main/java/net/pterodactylus/sone/main/SonePlugin.java
+++ src/main/java/net/pterodactylus/sone/main/SonePlugin.java
@@ -116,7 +116,7 @@ public class SonePlugin implements FredPlugin, FredPluginFCP, FredPluginL10n, Fr
 	}
 
 	/** The version. */
-	public static final Version VERSION = new Version(0, 9, 4);
+	public static final Version VERSION = new Version(0, 9, 5);
 
 	/** The current year at time of release. */
 	private static final int YEAR = 2016;
