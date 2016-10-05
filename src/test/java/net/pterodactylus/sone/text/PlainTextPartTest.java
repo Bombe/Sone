8bca76f6a624a0f0f9576ffa73bf55688149e377
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2016-10-05 21:08:24 +0200

    Add test for PlainTextPart

---
+++ src/test/java/net/pterodactylus/sone/text/PlainTextPartTest.java
@@ -0,0 +1,22 @@
+package net.pterodactylus.sone.text;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Test;
+
+/**
+ * Unit test for {@link PlainTextPart}.
+ *
+ * @author <a href="mailto:bombe@pterodactylus.net">David ‘Bombe’ Roden</a>
+ */
+public class PlainTextPartTest {
+
+	private final PlainTextPart part = new PlainTextPart("text");
+
+	@Test
+	public void textIsRetainedCorrectly() {
+		assertThat(part.getText(), is("text"));
+	}
+
+}
