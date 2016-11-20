7a70c5564da777276295d6ec29ab76446425bb36
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2016-11-20 01:14:04 +0100

    Add some logging to element loader

--- src/main/kotlin/net/pterodactylus/sone/core/DefaultElementLoader.kt
+++ src/main/kotlin/net/pterodactylus/sone/core/DefaultElementLoader.kt
@@ -20,27 +20,32 @@ class DefaultElementLoader(private val freenetInterface: FreenetInterface, ticke
 	private val imageCache = CacheBuilder.newBuilder().build<String, LinkedElement>()
 	private val callback = object : FreenetInterface.BackgroundFetchCallback {
 		override fun cancelForMimeType(uri: FreenetURI, mimeType: String): Boolean {
+			println("Got MIME Type “$mimeType” for $uri.")
 			return !mimeType.startsWith("image/")
 		}
 
 		override fun loaded(uri: FreenetURI, mimeType: String, data: ByteArray) {
+			println("Got ${data.size} Bytes with MIME Type $mimeType for $uri.")
 			if (!mimeType.startsWith("image/")) {
 				return
 			}
 			ByteArrayInputStream(data).use {
 				ImageIO.read(it)
 			}?.let {
+				println("Successfully parsed images from $uri.")
 				imageCache.get(uri.toString()) { LinkedElement(uri.toString()) }
 			}
 			removeLoadingLink(uri)
 		}
 
 		override fun failed(uri: FreenetURI) {
+			println("Failed to load $uri.")
 			failureCache.put(uri.toString(), true)
 			removeLoadingLink(uri)
 		}
 
 		private fun removeLoadingLink(uri: FreenetURI) {
+			println("Not loading anymore: $uri.")
 			synchronized(loadingLinks) {
 				loadingLinks.invalidate(uri.toString())
 			}
@@ -48,18 +53,23 @@ class DefaultElementLoader(private val freenetInterface: FreenetInterface, ticke
 	}
 
 	override fun loadElement(link: String): LinkedElement {
+		println("Checking for $link...")
 		synchronized(loadingLinks) {
 			imageCache.getIfPresent(link)?.run {
+				println("In the Image Cache: $link")
 				return this
 			}
 			failureCache.getIfPresent(link)?.run {
+				println("In the Failure Cache: $link")
 				return LinkedElement(link, failed = true)
 			}
 			if (loadingLinks.getIfPresent(link) == null) {
+				println("Not loading: $link")
 				loadingLinks.put(link, true)
 				freenetInterface.startFetch(FreenetURI(link), callback)
 			}
 		}
+		println("Returning loading element: $link")
 		return LinkedElement(link, loading = true)
 	}
 
