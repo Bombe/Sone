0c6f4b61672757f062ce93794e2dc8f558b2913b
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2017-03-24 19:27:54 +0100

    Replace dismiss notification page with Kotlin version

---
+++ src/main/kotlin/net/pterodactylus/sone/web/DismissNotificationPage.kt
@@ -0,0 +1,20 @@
+package net.pterodactylus.sone.web
+
+import net.pterodactylus.sone.web.page.FreenetRequest
+import net.pterodactylus.util.template.Template
+import net.pterodactylus.util.template.TemplateContext
+
+/**
+ * Page that lets the user dismiss a notification.
+ */
+class DismissNotificationPage(template: Template, webInterface: WebInterface):
+		SoneTemplatePage("dismissNotification.html", template, "Page.DismissNotification.Title", webInterface) {
+
+	override fun handleRequest(request: FreenetRequest, templateContext: TemplateContext) {
+		val returnPage = request.httpRequest.getPartAsStringFailsafe("returnPage", 256)
+		val notificationId = request.httpRequest.getPartAsStringFailsafe("notification", 36)
+		webInterface.getNotification(notificationId).orNull()?.takeIf { it.isDismissable }?.dismiss()
+		throw RedirectException(returnPage)
+	}
+
+}
