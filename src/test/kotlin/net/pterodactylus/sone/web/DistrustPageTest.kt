ba52e76733494d39ea693d51fdb9a5f14eb10674
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2017-03-24 20:00:11 +0100

    Add some tests

--- src/test/kotlin/net/pterodactylus/sone/web/DistrustPageTest.kt
+++ src/test/kotlin/net/pterodactylus/sone/web/DistrustPageTest.kt
@@ -2,19 +2,38 @@ package net.pterodactylus.sone.web
 
 import net.pterodactylus.sone.data.Sone
 import net.pterodactylus.sone.test.mock
+import net.pterodactylus.sone.test.whenever
 import net.pterodactylus.util.web.Method.GET
 import net.pterodactylus.util.web.Method.POST
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.equalTo
 import org.junit.Test
 import org.mockito.Mockito.verify
 
 /**
  * Unit test for [DistrustPage].
  */
-class DistrustPageTest : WebPageTest() {
+class DistrustPageTest: WebPageTest() {
 
 	private val page = DistrustPage(template, webInterface)
 
 	override fun getPage() = page
+
+	@Test
+	fun `page returns correct path`() {
+		assertThat(page.path, equalTo("distrust.html"))
+	}
+
+	@Test
+	fun `page requires login`() {
+		assertThat(page.requiresLogin(), equalTo(true))
+	}
+
+	@Test
+	fun `page returns correct title`() {
+		whenever(l10n.getString("Page.Distrust.Title")).thenReturn("distrust page title")
+		assertThat(page.getPageTitle(freenetRequest), equalTo("distrust page title"))
+	}
 
 	@Test
 	fun `get request does not redirect`() {
