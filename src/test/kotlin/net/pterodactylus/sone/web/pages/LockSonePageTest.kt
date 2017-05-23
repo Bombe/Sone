4e8ae410b3b366432367d86863685406334cc4be
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2017-05-23 20:18:09 +0200

    Add more tests for lock Sone page

--- src/test/kotlin/net/pterodactylus/sone/web/pages/LockSonePageTest.kt
+++ src/test/kotlin/net/pterodactylus/sone/web/pages/LockSonePageTest.kt
@@ -2,8 +2,8 @@ package net.pterodactylus.sone.web.pages
 
 import net.pterodactylus.sone.data.Sone
 import net.pterodactylus.sone.test.mock
-import net.pterodactylus.sone.web.pages.LockSonePage
-import net.pterodactylus.sone.web.pages.WebPageTest
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.equalTo
 import org.junit.Test
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.never
@@ -17,6 +17,22 @@ class LockSonePageTest : WebPageTest() {
 	private val page = LockSonePage(template, webInterface)
 
 	override fun getPage() = page
+
+	@Test
+	fun `page returns correct path`() {
+		assertThat(page.path, equalTo("lockSone.html"))
+	}
+
+	@Test
+	fun `page requires login`() {
+	    assertThat(page.requiresLogin(), equalTo(false))
+	}
+
+	@Test
+	fun `page returns correct title`() {
+	    addTranslation("Page.LockSone.Title", "lock Sone page title")
+		assertThat(page.getPageTitle(freenetRequest), equalTo("lock Sone page title"))
+	}
 
 	@Test
 	fun `locking an invalid local sone redirects to return page`() {
