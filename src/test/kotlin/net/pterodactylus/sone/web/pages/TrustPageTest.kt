2cdde02993e81e73a3b7edc3bcee13f56b43d6d5
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2017-06-03 17:42:26 +0200

    Add more tests for trust page

--- src/test/kotlin/net/pterodactylus/sone/web/pages/TrustPageTest.kt
+++ src/test/kotlin/net/pterodactylus/sone/web/pages/TrustPageTest.kt
@@ -3,6 +3,8 @@ package net.pterodactylus.sone.web.pages
 import net.pterodactylus.sone.data.Sone
 import net.pterodactylus.sone.test.mock
 import net.pterodactylus.util.web.Method.POST
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.equalTo
 import org.junit.Test
 import org.mockito.ArgumentMatchers.any
 import org.mockito.ArgumentMatchers.eq
@@ -19,8 +21,33 @@ class TrustPageTest : WebPageTest() {
 	override fun getPage() = page
 
 	@Test
+	fun `page returns correct path`() {
+	    assertThat(page.path, equalTo("trust.html"))
+	}
+
+	@Test
+	fun `page requires login`() {
+	    assertThat(page.requiresLogin(), equalTo(true))
+	}
+
+	@Test
+	fun `page returns correct title`() {
+	    addTranslation("Page.Trust.Title", "title trust page")
+		assertThat(page.getPageTitle(freenetRequest), equalTo("title trust page"))
+	}
+
+	@Test
 	fun `get method does not redirect`() {
 		verifyNoRedirect { }
+	}
+
+	@Test
+	fun `post request without sone redirects to return page`() {
+		setMethod(POST)
+		addHttpRequestPart("returnPage", "return.html")
+		verifyRedirect("return.html") {
+			verify(core, never()).trustSone(eq(currentSone), any())
+		}
 	}
 
 	@Test
