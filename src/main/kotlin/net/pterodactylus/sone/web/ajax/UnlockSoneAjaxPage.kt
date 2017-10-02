8419f82ea9a6da6f4c8c564e13a2614be89ae3df
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2017-10-02 15:59:35 +0200

    Replace unlock Sone ajax page with Kotlin version

---
+++ src/main/kotlin/net/pterodactylus/sone/web/ajax/UnlockSoneAjaxPage.kt
@@ -0,0 +1,22 @@
+package net.pterodactylus.sone.web.ajax
+
+import net.pterodactylus.sone.utils.parameters
+import net.pterodactylus.sone.web.WebInterface
+import net.pterodactylus.sone.web.page.FreenetRequest
+
+/**
+ * Lets the user [unlock][net.pterodactylus.sone.core.Core.unlockSone] a [Sone][net.pterodactylus.sone.data.Sone].
+ */
+class UnlockSoneAjaxPage(webInterface: WebInterface) : JsonPage("unlockSone.ajax", webInterface) {
+
+	override fun requiresLogin() = false
+
+	override fun createJsonObject(request: FreenetRequest) =
+			request.parameters["sone"]
+					?.let(webInterface.core::getLocalSone)
+					?.also(webInterface.core::unlockSone)
+					?.also { webInterface.core.touchConfiguration() }
+					?.let { createSuccessJsonObject() }
+					?: createErrorJsonObject("invalid-sone-id")
+
+}
