a14d09dbe4e09ad923abe7cc21957dfa52a02e7a
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2017-10-13 08:01:59 +0200

    Add default value for “trusted” parameter

--- src/main/kotlin/net/pterodactylus/sone/text/FreenetLinkPart.kt
+++ src/main/kotlin/net/pterodactylus/sone/text/FreenetLinkPart.kt
@@ -5,7 +5,7 @@ package net.pterodactylus.sone.text
  * link is an SSK or USK link and the post was created by an identity that owns
  * the keyspace in question.
  */
-data class FreenetLinkPart(val link: String, override val text: String, val title: String, val trusted: Boolean) : Part {
+data class FreenetLinkPart(val link: String, override val text: String, val title: String, val trusted: Boolean = false) : Part {
 
 	constructor(link: String, text: String, trusted: Boolean) : this(link, text, link, trusted)
 
