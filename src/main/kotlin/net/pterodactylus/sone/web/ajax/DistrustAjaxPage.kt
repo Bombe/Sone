210f03a3afc928e3bdba697957daa35bd8ffe4e5
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2018-01-22 07:13:51 +0100

    Add test for DI constructability of DistrustAjaxPage

--- src/main/kotlin/net/pterodactylus/sone/web/ajax/DistrustAjaxPage.kt
+++ src/main/kotlin/net/pterodactylus/sone/web/ajax/DistrustAjaxPage.kt
@@ -6,13 +6,15 @@ import net.pterodactylus.sone.utils.let
 import net.pterodactylus.sone.utils.parameters
 import net.pterodactylus.sone.web.WebInterface
 import net.pterodactylus.sone.web.page.FreenetRequest
+import javax.inject.Inject
 
 /**
  * AJAX page that lets the user distrust a Sone.
  *
  * @see Core.distrustSone(Sone, Sone)
  */
-class DistrustAjaxPage(webInterface: WebInterface) : LoggedInJsonPage("distrustSone.ajax", webInterface) {
+class DistrustAjaxPage @Inject constructor(webInterface: WebInterface) :
+		LoggedInJsonPage("distrustSone.ajax", webInterface) {
 
 	override fun createJsonObject(currentSone: Sone, request: FreenetRequest) =
 			request.parameters["sone"]
--- src/test/kotlin/net/pterodactylus/sone/web/ajax/DistrustAjaxPageTest.kt
+++ src/test/kotlin/net/pterodactylus/sone/web/ajax/DistrustAjaxPageTest.kt
@@ -1,9 +1,12 @@
 package net.pterodactylus.sone.web.ajax
 
 import net.pterodactylus.sone.data.Sone
+import net.pterodactylus.sone.test.getInstance
 import net.pterodactylus.sone.test.mock
+import net.pterodactylus.sone.web.baseInjector
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.equalTo
+import org.hamcrest.Matchers.notNullValue
 import org.junit.Test
 import org.mockito.Mockito.verify
 
@@ -40,6 +43,11 @@ class DistrustAjaxPageTest : JsonPageTest("distrustSone.ajax", pageSupplier = ::
 		addRequestParameter("sone", "sone-id")
 		assertThatJsonIsSuccessful()
 		assertThat(json["trustValue"]?.asInt(), equalTo(-33))
+	}
+
+	@Test
+	fun `page can be created by dependency injection`() {
+	    assertThat(baseInjector.getInstance<DistrustAjaxPage>(), notNullValue())
 	}
 
 }
