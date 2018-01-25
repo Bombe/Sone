ae78f11f111ddc964a36cc25ab6da413685d69a6
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2018-01-25 19:17:01 +0100

    Add test for DI constructability of GetTranslationAjaxPage

--- src/main/kotlin/net/pterodactylus/sone/web/ajax/GetTranslationAjaxPage.kt
+++ src/main/kotlin/net/pterodactylus/sone/web/ajax/GetTranslationAjaxPage.kt
@@ -3,11 +3,13 @@ package net.pterodactylus.sone.web.ajax
 import net.pterodactylus.sone.utils.parameters
 import net.pterodactylus.sone.web.WebInterface
 import net.pterodactylus.sone.web.page.FreenetRequest
+import javax.inject.Inject
 
 /**
  * Returns the translation for a given key as JSON object.
  */
-class GetTranslationAjaxPage(webInterface: WebInterface) : JsonPage("getTranslation.ajax", webInterface) {
+class GetTranslationAjaxPage @Inject constructor(webInterface: WebInterface) :
+		JsonPage("getTranslation.ajax", webInterface) {
 
 	override val needsFormPassword = false
 	override val requiresLogin = false
