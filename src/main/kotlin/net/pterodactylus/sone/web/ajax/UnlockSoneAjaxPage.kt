f7b783ac48b77d0f2b258e3efa7e853388165247
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2018-01-25 19:28:49 +0100

    Add test for DI constructability of UnlockSoneAjaxPage

--- src/main/kotlin/net/pterodactylus/sone/web/ajax/UnlockSoneAjaxPage.kt
+++ src/main/kotlin/net/pterodactylus/sone/web/ajax/UnlockSoneAjaxPage.kt
@@ -3,11 +3,13 @@ package net.pterodactylus.sone.web.ajax
 import net.pterodactylus.sone.utils.parameters
 import net.pterodactylus.sone.web.WebInterface
 import net.pterodactylus.sone.web.page.FreenetRequest
+import javax.inject.Inject
 
 /**
  * Lets the user [unlock][net.pterodactylus.sone.core.Core.unlockSone] a [Sone][net.pterodactylus.sone.data.Sone].
  */
-class UnlockSoneAjaxPage(webInterface: WebInterface) : JsonPage("unlockSone.ajax", webInterface) {
+class UnlockSoneAjaxPage @Inject constructor(webInterface: WebInterface) :
+		JsonPage("unlockSone.ajax", webInterface) {
 
 	override val requiresLogin = false
 
