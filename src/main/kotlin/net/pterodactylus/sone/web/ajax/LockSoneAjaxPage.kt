10f11f421c611324fb8273afe917fd31683bb3e0
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2018-01-25 19:20:19 +0100

    Add test for DI constructability of LockSoneAjaxPage

--- src/main/kotlin/net/pterodactylus/sone/web/ajax/LockSoneAjaxPage.kt
+++ src/main/kotlin/net/pterodactylus/sone/web/ajax/LockSoneAjaxPage.kt
@@ -3,11 +3,13 @@ package net.pterodactylus.sone.web.ajax
 import net.pterodactylus.sone.utils.parameters
 import net.pterodactylus.sone.web.WebInterface
 import net.pterodactylus.sone.web.page.FreenetRequest
+import javax.inject.Inject
 
 /**
  * Lets the user [lock][net.pterodactylus.sone.core.Core.lockSone] a [Sone][net.pterodactylus.sone.data.Sone].
  */
-class LockSoneAjaxPage(webInterface: WebInterface) : JsonPage("lockSone.ajax", webInterface) {
+class LockSoneAjaxPage @Inject constructor(webInterface: WebInterface) :
+		JsonPage("lockSone.ajax", webInterface) {
 
 	override val requiresLogin = false
 
