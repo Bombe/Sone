3f9aed74383ed43027dad4eb9a12eec88d7c479e
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2019-04-14 00:06:06 +0200

    🎨 Replace FreenetRequest with Kotlin version

---
+++ src/main/kotlin/net/pterodactylus/sone/web/page/FreenetRequest.kt
@@ -0,0 +1,25 @@
+/*
+ * Sone - FreenetRequest.java - Copyright © 2011–2019 David Roden
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.pterodactylus.sone.web.page
+
+import freenet.clients.http.*
+import freenet.support.api.*
+import net.pterodactylus.util.web.*
+import java.net.*
+
+class FreenetRequest(uri: URI, method: Method, val httpRequest: HTTPRequest, val toadletContext: ToadletContext) : Request(uri, method)
