19486ba47204d8554c04e1355dae6c113aa1e6c4
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2019-04-14 00:01:02 +0200

    🎨 Replace FreenetRequestTest with Kotlin version

---
+++ src/test/kotlin/net/pterodactylus/sone/web/page/FreenetRequestTest.kt
@@ -0,0 +1,40 @@
+package net.pterodactylus.sone.web.page
+
+import freenet.clients.http.*
+import freenet.support.api.*
+import net.pterodactylus.util.web.*
+import org.hamcrest.MatcherAssert.*
+import org.hamcrest.Matchers.*
+import org.junit.*
+import org.mockito.Mockito.*
+import java.net.*
+
+class FreenetRequestTest {
+
+	private val uri = URI(".")
+	private val method = Method.GET
+	private val httpRequest = mock(HTTPRequest::class.java)
+	private val toadletContext = mock(ToadletContext::class.java)
+	private val request = FreenetRequest(uri, method, httpRequest, toadletContext)
+
+	@Test
+	fun `uri is retained correctly`() {
+		assertThat(request.uri, equalTo(uri))
+	}
+
+	@Test
+	fun `method is retained correctly`() {
+		assertThat(request.method, equalTo(method))
+	}
+
+	@Test
+	fun `http request is retained correctly`() {
+		assertThat(request.httpRequest, equalTo(httpRequest))
+	}
+
+	@Test
+	fun `toadlet context is retained correctly`() {
+		assertThat(request.toadletContext, equalTo(toadletContext))
+	}
+
+}
