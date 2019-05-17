fc75cb21077352b708d8701708dff8f5c2f2cdde
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2019-05-17 16:53:24 +0200

    🚧 Add annotation for the template path

--- src/main/kotlin/net/pterodactylus/sone/web/page/MenuName.kt
+++ src/main/kotlin/net/pterodactylus/sone/web/page/MenuName.kt
@@ -5,3 +5,7 @@ import net.pterodactylus.util.web.*
 annotation class MenuName(val value: String)
 
 val Page<*>.menuName get() = javaClass.getAnnotation(MenuName::class.java)?.value
+
+annotation class TemplatePath(val value: String)
+
+val Page<*>.templatePath get() = javaClass.getAnnotation(TemplatePath::class.java)?.value
