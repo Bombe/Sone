4e14522463e25ae70756bac80a507d01a772a0c5
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2019-11-01 18:33:17 +0100

    ✨ Add boolean helper onFalse

--- src/test/kotlin/net/pterodactylus/sone/utils/BooleansTest.kt
+++ src/test/kotlin/net/pterodactylus/sone/utils/BooleansTest.kt
@@ -1,9 +1,8 @@
 package net.pterodactylus.sone.utils
 
-import org.hamcrest.MatcherAssert.assertThat
-import org.hamcrest.Matchers.equalTo
-import org.hamcrest.Matchers.nullValue
-import org.junit.Test
+import org.hamcrest.MatcherAssert.*
+import org.hamcrest.Matchers.*
+import kotlin.test.*
 
 /**
  * Unit test for [Booleans].
@@ -28,6 +27,26 @@ class BooleansTest {
 	@Test
 	fun `ifFalse is not executed if boolean is true`() {
 		assertThat(true.ifFalse { true }, nullValue())
+	}
+
+	@Test
+	fun `onFalse returns true on true`() {
+		assertThat(true.onFalse {}, equalTo(true))
+	}
+
+	@Test
+	fun `onFalse returns false on false`() {
+		assertThat(false.onFalse {}, equalTo(false))
+	}
+
+	@Test
+	fun `onFalse is not executed on true`() {
+		assertThat(true.onFalse { throw RuntimeException() }, equalTo(true))
+	}
+
+	@Test(expected = RuntimeException::class)
+	fun `onFalse is executed on false`() {
+		false.onFalse { throw RuntimeException() }
 	}
 
 }
