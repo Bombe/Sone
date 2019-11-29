188857148a7b035bfc6033b1fd40d774bd1fac65
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2019-11-29 21:45:28 +0100

    🚧 Add handler for “new Sone found” event

---
+++ src/main/kotlin/net/pterodactylus/sone/web/notification/NewSoneHandler.kt
@@ -0,0 +1,45 @@
+/**
+ * Sone - NewSoneHandler.kt - Copyright © 2019 David ‘Bombe’ Roden
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.pterodactylus.sone.web.notification
+
+import com.google.common.eventbus.*
+import net.pterodactylus.sone.core.event.*
+import net.pterodactylus.sone.data.*
+import net.pterodactylus.sone.notify.*
+import net.pterodactylus.util.notify.*
+import net.pterodactylus.util.template.*
+
+/**
+ * Notification handler for “new Sone discovered” events.
+ */
+class NewSoneHandler(private val notificationManager: NotificationManager, template: Template) {
+
+	private val notification = ListNotification<Sone>("new-sone-notification", "", template)
+
+	@Subscribe
+	fun newSoneFound(newSoneFoundEvent: NewSoneFoundEvent) {
+		if (!notificationManager.hasNotification("first-start-notification")) {
+			notification.add(newSoneFoundEvent.sone)
+			notificationManager.addNotification(notification)
+		}
+	}
+
+}
+
+private fun NotificationManager.hasNotification(id: String) =
+		getNotification(id) != null
---
+++ src/test/kotlin/net/pterodactylus/sone/web/notification/NewSoneHandlerTest.kt
@@ -0,0 +1,68 @@
+/**
+ * Sone - NewSoneHandlerTest.kt - Copyright © 2019 David ‘Bombe’ Roden
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.pterodactylus.sone.web.notification
+
+import com.google.common.eventbus.*
+import net.pterodactylus.sone.core.event.*
+import net.pterodactylus.sone.data.impl.*
+import net.pterodactylus.sone.notify.*
+import net.pterodactylus.util.notify.*
+import net.pterodactylus.util.template.*
+import org.hamcrest.MatcherAssert.*
+import org.hamcrest.Matchers.*
+import java.io.*
+import kotlin.test.*
+
+class NewSoneHandlerTest {
+
+	@Suppress("UnstableApiUsage")
+	private val eventBus = EventBus()
+	private val notificationManager = NotificationManager()
+	private val handler = NewSoneHandler(notificationManager, Template())
+
+	init {
+		eventBus.register(handler)
+	}
+
+	@Test
+	fun `handler adds notification if new sone event is fired`() {
+		eventBus.post(NewSoneFoundEvent(sone))
+		val notification = notificationManager.notifications.single() as ListNotification<*>
+		assertThat(notification.id, equalTo("new-sone-notification"))
+	}
+
+	@Test
+	fun `handler adds sone to notification`() {
+		eventBus.post(NewSoneFoundEvent(sone))
+		val notification = notificationManager.notifications.single() as ListNotification<*>
+		assertThat(notification.elements, contains<Any>(sone))
+	}
+
+	@Test
+	fun `handler does not add notification on new sone event if first-start notification is present`() {
+		notificationManager.addNotification(object : AbstractNotification("first-start-notification") {
+			override fun render(writer: Writer) = Unit
+		})
+		eventBus.post(NewSoneFoundEvent(sone))
+		val notification = notificationManager.notifications.single()
+		assertThat(notification.id, equalTo("first-start-notification"))
+	}
+
+}
+
+private val sone = IdOnlySone("sone-id")
