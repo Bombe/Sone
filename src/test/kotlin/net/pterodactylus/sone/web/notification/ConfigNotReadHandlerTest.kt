e6aa326d36fc1d653ed5473fd02af8cba8992b9a
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2019-12-11 18:04:20 +0100

    🚧 Add handler for config-not-read notification

---
+++ src/test/kotlin/net/pterodactylus/sone/web/notification/ConfigNotReadHandlerTest.kt
@@ -0,0 +1,48 @@
+/**
+ * Sone - ConfigNotReadHandler.kt - Copyright © 2019 David ‘Bombe’ Roden
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.pterodactylus.sone.web.notification
+
+import com.google.common.eventbus.*
+import net.pterodactylus.sone.core.event.*
+import net.pterodactylus.util.notify.*
+import net.pterodactylus.util.template.*
+import org.hamcrest.MatcherAssert.*
+import org.hamcrest.Matchers.*
+import org.junit.*
+
+/**
+ * Unit test for [ConfigNotReadHandler].
+ */
+@Suppress("UnstableApiUsage")
+class ConfigNotReadHandlerTest {
+
+	private val eventBus = EventBus()
+	private val notificationManager = NotificationManager()
+	private val notification = TemplateNotification("", Template())
+
+	init {
+		eventBus.register(ConfigNotReadHandler(notificationManager, notification))
+	}
+
+	@Test
+	fun `handler adds notification to manager when config was not read`() {
+		eventBus.post(ConfigNotRead())
+		assertThat(notificationManager.notifications, contains<Notification>(notification))
+	}
+
+}
